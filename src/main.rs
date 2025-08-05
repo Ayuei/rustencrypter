b281432074df3976a0f554a1be7924e2b63f95ad
Vincent Nguyen <18545131+Ayuei@users.noreply.github.com>
2025-08-05 20:49:35 +1000

    Simplify main logic

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-use rustencrypter::{run, Opt, encrypt_cb, decrypt_cb};
+use rustencrypter::run;
 use std::process;
-use std::error;
-use structopt::StructOpt;
 
-fn main() {
-    let opt = Opt::from_args();
-
-    let cb: &dyn Fn(&mut Vec<u8>, &Vec<u8>) -> Result<Vec<u8>, Box<dyn error::Error>>  = {
-        if opt.encrypt {
-            &encrypt_cb
-        } else {
-            &decrypt_cb
-        }
-    };
-
-    match run(opt, cb) {
-        Ok(data) => data,
-        Err(error) => {
-            println!("{:?}", error);
-            process::exit(1);
-        }
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = run(clap::Parser::parse()) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
+    Ok(())
 }
